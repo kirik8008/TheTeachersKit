aaa800f0f7c702ad091e876c679afb58ea776e12
Smoks <dd>
2017-08-31 12:08:27 +0300

    170831-11
    + Частичная защита POST запросов.
    + Добавлена страница профиля.
    + Возможность смены пароля, добавления телефона и почты в профиль.
    + Реализована функция ухода от старого хеширования паролей MD5, в
    следующий обновлениях будет полный отказ от md5.

--- application/views/index.php
+++ application/views/index.php
@@ -4,13 +4,8 @@
                 <table WIDTH="100%">
                 <tr><td><h2 style="margin-top:0;">The Teacher's Kit</h2> 
                                <p>УчительскийКомплект - CMS для контроля выдачи оборудования учителям. </p>
-<<<<<<< HEAD
-                  <div class="alert alert-info"><center>Актуальные версии на GitHub</center></div>
+                  <div class="alert alert-info"><center>Версия для демонстрации функций CMS</center></div>
               </td><td WIDTH="10%"><center><i style="font-size: 100px">β</i></center></td>
-=======
-                  <div class="alert alert-info"><center>Версия для демонстрации функций CMS (170725-13)</center></div>
-              </td><td WIDTH="10%"><center><i style="font-size: 100px">α</i></center></td>
->>>>>>> master
                </tr>
                </table>
                </div>
