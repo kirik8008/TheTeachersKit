b3943635c6d635f70e2383f37fe10d562b6badd2
Smoks <dd>
2017-09-04 10:06:59 +0300

    170904-10
    Добавление в исключения

--- application/config/config.php
+++ application/config/config.php
@@ -453,7 +453,7 @@ $config['csrf_token_name'] = 'csrf_test_name';
 $config['csrf_cookie_name'] = 'csrf_cookie_name';
 $config['csrf_expire'] = 7200;
 $config['csrf_regenerate'] = TRUE;
-$config['csrf_exclude_uris'] = array('kit/jpost','info/test');
+$config['csrf_exclude_uris'] = array('kit/jpost','info/test','teacher/view/[0-9]+');
 
 /*
 |--------------------------------------------------------------------------
