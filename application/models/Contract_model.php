76886cfd3bfeec8ada955eae417227b8459599be
Smoks <dd>
2017-09-19 16:52:06 +0300

    170919-16
    Новый раздел меню: Договоры, в котором есть информация по всем
    заключенным договорам, а также информация по договорам, срок действия
    которых истек.

---
+++ application/models/Contract_model.php
@@ -0,0 +1,72 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class contract_model extends CI_Model {
+	
+
+	public function __construct()
+		{
+			
+		}
+		
+	//----------------------------------------------------------	
+		
+	public function permanent_teachers_equipment() // постоянные преподаватели с оборудованием (только подсчет)
+		{
+			$where=array('work'=>'0','job'=>'1');
+			$this->db->where($where);
+			$pte=$this->db->count_all_results('educator');
+			return $pte;
+		}
+		
+	public function temporary_teachers_equipment() // временные преподаватели с оборудованием (только подсчет)
+		{
+			$where=array('work'=>'1','job'=>'1');
+			$this->db->where($where);
+			$tte=$this->db->count_all_results('educator');
+			return $tte;
+		}
+		
+	public function temporary_expired_contract() // временные преподаватели с просроченными договорами (только подсчет)
+		{
+			$date_temp=explode('-',date("Y-m-d"));
+			if($date_temp[1]<6) $date_temp[0]=$date_temp[0]-1; // находим год начала учебного года.
+			$where=array('work'=>'1','job'=>'1','contract_date <'=>$date_temp[0].'-05-31'); // выводим информацию о заключенных ранее 31 мая **** года
+			$this->db->where($where);
+			$tec=$this->db->count_all_results('educator');
+			return $tec;
+		}
+		
+	public function contract_teachers($num,$offset) // сбор массива для страницы: краткая информация по договорам
+		{
+			$return['pte']=$this->permanent_teachers_equipment();
+			$return['tte']=$this->temporary_teachers_equipment();
+			$return['tec']=$this->temporary_expired_contract();
+			if ($return['tec']>0) $return['tec_info']=$this->info_temporary_expired_contract();
+			$return['contract']=$this->all_signed_contract($num,$offset);
+			return $return;
+		}
+		
+	public function info_temporary_expired_contract()
+		{
+			$date_temp=explode('-',date("Y-m-d"));
+			if($date_temp[1]<6) $date_temp[0]=$date_temp[0]-1; // находим год начала учебного года.
+			$where=array('work'=>'1','job'=>'1','contract_date <'=>$date_temp[0].'-05-31'); // выводим информацию о заключенных ранее 31 мая **** года
+			$this->db->select('id,surname,realname,middlename,telephone,contract,contract_date'); // выводим только нужную информацию.
+			$this->db->where($where);
+			$dbase=$this->db->get('educator');
+			$info=$dbase->result_array();
+			return $info;
+		}
+	
+	public function all_signed_contract($num,$offset) // вывод всех договоров: заключенный и просроченных
+		{
+			$where=array('job'=>'1','contract !='=>'0');
+			$this->db->where($where);
+			$this->db->order_by('contract_date');
+			$temp=$this->db->get('educator',$num,$offset);
+			$result=$temp->result_array();
+			return $result;
+		}
+			
+}
---
+++ application/views/contract_view.php
@@ -0,0 +1,60 @@
+
+        <div id="global">
+            <div class="container-fluid cm-container-white">
+                  <h2 style="margin-top:0;">Краткая информация по договорам</h2> 
+             <table class="table table-hover" WIDTH="">
+             <tr><td>Постоянные преподователи с оборудованием</td><td><span class="badge"><?=$pte;?></span> человек</td></tr>
+             <tr><td>Временные преподователи с оборудованием</td><td><span class="badge"><?=$tte; ?></span> человек</td></tr>
+             <tr><td>
+             	<? if(!empty($tec_info)) {
+             		?>
+             		<div class="panel-group" id="accordion">
+             		<div>
+    					<div>
+								<a data-toggle="collapse" data-parent="#accordion" href="#TTE">
+          						Показать договора подлежащие перезаключению или изъятию оборудования
+        						</a>
+    					</div>
+    					<div id="TTE" class="panel-collapse collapse">
+      						<div class="panel-body">
+      							<table class="table table-hover">
+             					<? foreach($tec_info as $tc): ?>
+             					<tr><td><?=$tc['surname'];?> <?=$tc['realname'];?> <?=$tc['middlename'];?> <? if(!empty($tc['telephone'])) echo '(т.'.$tc['telephone'].')'; ?></td><td><span class="label label-danger">Истек срок действия договора</span> № <?=$tc['contract'];?> (от <?=$tc['contract_date'];?>).
+             					<td><a href="<?=base_url();?>teacher/view/<?=$tc['id'];?>" class="tooltip-test" data-toggle="tooltip" data-placement="top" title="Показать данные"><img src="<?=base_url();?>graphics/img/sf/user-id.svg" height="24" width="24"></a></td>
+             					</td></tr> <? endforeach; ?></table>
+        			     	</div>
+    					</div>
+ 					</div>
+					</div>
+             		<?
+             	 } else echo 'Договора подлежащие перезаключению или изъятию оборудования'; ?>
+             	</td><td><span class="badge"><?=$tec;?></span> человек</td></tr>
+
+               </table>
+               </div>
+            <div class="container-fluid">
+            	<div class="panel panel-default">
+                    <div class="panel-heading">Все преподаватели с заключенными договорами<sup><small>*</small></sup> </div>
+                    	<div class="panel-body" id="demo-buttons">
+                        	<table class="table table-hover">
+                        		<tr><th></th><th>Преподаватель</th><th>Договор</th><th></th></tr>
+                        		<? if (empty($contract)) {?>
+                        		<tr><td COLSPAN=7><center>В базе нет учителей!</center></td></tr>
+                        		
+                        		<? } else {?>
+                        		<? foreach($contract as $item): ?>
+                        		<? if($item['work_source']==0) echo '<tr>'; else echo '<tr class="info">'; ?><td><img src="<?=$item['photo'];?>" width="32" height="32" class="img-circle"></td><td><?=$item['surname'];?> <?=$item['realname'];?> <?=$item['middlename'];?></td><td><? if($item['contract']!='0') echo '<b>'.$item['contract'].'</b> (от '.$item['contract_date']; else echo 'Договора нет';?> <? if($item['work_source']=='1') echo 'сроком до 31 мая.)'; else echo 'бессрочный)';?></td><td><a href="<?=base_url();?>teacher/view/<?=$item['id'];?>" class="tooltip-test" data-toggle="tooltip" data-placement="top" title="Показать данные <?=$item['surname'];?> <?=$item['realname'];?>"><img src="<?=base_url();?>graphics/img/sf/user-id.svg" height="24" width="24"></a></td></tr>
+                        		<? endforeach; ?>
+                        		<? } ?>
+                        	</table>
+                        	<? echo $this->pagination->create_links(); ?>
+                        	 <br><sup><small>*</small></sup> - Договора собранные скриптом на момент выдачи оборудования преподователю через T.T.K (The Teacher's Kit)
+
+                        </div>
+                        </div>
+               
+                
+                 
+            </div>
+            
+            
