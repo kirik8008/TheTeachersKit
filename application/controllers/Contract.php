7e12f00135c1d290b11b6174fb40c230021123e4
Smoks <dd>
2017-09-19 16:47:05 +0300

    170919-16
    New section menu: Contract, which has information on all the contracts
    concluded, as well as information on contracts which have expired.

---
+++ application/controllers/Contract.php
@@ -0,0 +1,54 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Contract extends CI_Controller {
+
+	public $data;
+
+	public function __construct()
+	{
+	parent::__construct(); // ОБЯЗАТЕЛЬНО
+		$this->data=array();
+		$this->load->model('Auth_model');
+		$this->data['user']=$this->Auth_model->authinfo;
+		$this->Auth_model->check_();
+		$this->load->model('contract_model');
+		$this->load->model('teacher_model');
+	} 
+
+public function all()
+	{
+		$where=array('job'=>'1','contract !='=>'0');
+		$this->db->where($where);
+		$config['base_url'] = base_url().'contract/all/';
+		$config['total_rows'] = $this->db->count_all_results('educator');
+		$config['per_page'] = 20;
+		$config['full_tag_open'] = '<center><ul class="pagination">';
+        $config['full_tag_close'] = '</ul></center>';
+        $config['num_tag_open'] = '<li>';
+        $config['num_tag_close'] = '</li>';
+        $config['cur_tag_open'] = '<li class="active"><span>';
+        $config['cur_tag_close'] = '<span class="sr-only">(current)</span></span></li>';
+        $config['prev_tag_open'] = '<li>';
+        $config['prev_tag_close'] = '</li>';
+        $config['next_tag_open'] = '<li>';
+        $config['next_tag_close'] = '</li>';
+        $config['first_link'] = '«';
+        $config['prev_link'] = '‹';
+        $config['last_link'] = '»';
+        $config['next_link'] = '›';
+    	$config['first_tag_open'] = '<li>';
+        $config['first_tag_close'] = '</li>';
+        $config['last_tag_open'] = '<li>';
+        $config['last_tag_close'] = '</li>';   
+		$this->pagination->initialize($config);
+		$data=$this->contract_model->contract_teachers($config['per_page'],$this->uri->segment(3));
+		$data['contract']=$this->teacher_model->handler_educator($data['contract']);
+		$this->load->view('menu',$this->data);
+		$this->load->view('contract_view',$data);
+		$this->load->view('footer');
+	}	
+	
+	
+	
+}
